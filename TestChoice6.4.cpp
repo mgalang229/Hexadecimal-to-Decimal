04c9688f6b0e0c9fedba2ba09a5a78577d12989f
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2019-11-09 17:52:51 +0800

    Add files via upload

---
+++ TestChoice6.4.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+	
+	int hexadecimal[100], i = 0, decimal = 0;
+	char num[255];
+	
+	cout << "~~~~~~~~~~ Hexadecimal Conversion ~~~~~~~~~~" << endl;
+	cout << "Enter Hexadecimal Number: ";
+	gets(num);
+	
+	while (num[i] > '\0'){
+	
+		if(num[i] == '1'){
+			hexadecimal[i] = 1;
+		} else if(num[i] == '2'){
+			hexadecimal[i] = 2;
+		} else if(num[i] == '3'){
+			hexadecimal[i] = 3;
+		} else if(num[i] == '4'){
+			hexadecimal[i] = 4;
+		} else if(num[i] == '5'){
+			hexadecimal[i] = 5;
+		} else if(num[i] == '6'){
+			hexadecimal[i] = 6;
+		} else if(num[i] == '7'){
+			hexadecimal[i] = 7;
+		} else if(num[i] == '8'){
+			hexadecimal[i] = 8;
+		} else if(num[i] == '9'){
+			hexadecimal[i] = 9;
+		} else if(num[i] == 'A'){
+			hexadecimal[i] = 10;
+		} else if(num[i] == 'B'){
+			hexadecimal[i] = 11;
+		} else if(num[i] == 'C'){
+			hexadecimal[i] = 12;
+		} else if(num[i] == 'D'){
+			hexadecimal[i] = 13;
+		} else if(num[i] == 'E'){
+			hexadecimal[i] = 14;
+		} else if(num[i] == 'F'){
+			hexadecimal[i] = 15;
+		} else{
+			cout << "Wrong Input";
+			return 0;
+		}
+		i++;
+	}
+	
+	int q = i-1;
+	cout << endl << "\t";
+	for(int k = 0; k < i; k++){
+		if(k != i-1){
+			cout << hexadecimal[k] << "*" << "(16^" << q << ") + ";
+		} else{
+			cout << hexadecimal[k] << "*" << "(16^" << q << ")";
+		}
+		decimal += hexadecimal[k] * pow(16, q);
+		q--;
+	}
+	cout << "\n\n\tDecimal Value = " << decimal;
+	
+	return 0;
+	
+}
